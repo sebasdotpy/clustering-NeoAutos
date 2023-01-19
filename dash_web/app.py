c68cab9629be3f3f6e6dd4a7af58f3574a9b37e8
Sebastian Aguirre <sebasaguirresolano@gmail.com>
2023-01-19 11:52:54 -0500

    prueba

--- dash_web/app.py
+++ dash_web/app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.graph_objects as go
 from waitress import serve
 
-# df = pd.read_csv('../data_clean.csv')
+df = pd.read_csv('../data_clean.csv')
 
 app = Dash(__name__)
 app.title = 'Clusterización'
@@ -13,14 +13,14 @@ server = app.server
 
 app.layout = html.Div([
     html.H1(children='Clusterización'),
-    # dcc.Graph(id='bar-plot',
-    #           figure=px.bar(df, y='price', x='Marca', color='Combustible', title='Bar plot: price vs Marca'),),
-    # dcc.Graph(id='box-plot',
-    #           figure=px.histogram(df, x='Marca')),
-    # dcc.Graph(id='hist-price',
-    #           figure=px.histogram(df, x='price', nbins=50))
+    dcc.Graph(id='bar-plot',
+              figure=px.bar(df, y='price', x='Marca', color='Combustible', title='Bar plot: price vs Marca'),),
+    dcc.Graph(id='box-plot',
+              figure=px.histogram(df, x='Marca')),
+    dcc.Graph(id='hist-price',
+              figure=px.histogram(df, x='price', nbins=50))
 ])
 
 if __name__=='__main__':
     # app.run_server(debug=True)
-    serve(server, port=8000)
+    serve(server, port=8050)
